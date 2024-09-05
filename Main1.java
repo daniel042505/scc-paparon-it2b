7dcd8f56e439d2ca8b51ad87ed599579fad65c2a
Dan <dandanhandsome1231@gmail.com>
2024-09-05 08:04:48 +0800

    SALARYEMPLOYEE

---
+++ Main1.java
@@ -0,0 +1,17 @@
+
+package main1;
+
+
+public class Main1 {
+
+    
+    public static void main(String[] args) {
+       
+      
+        Salaryy salaryCalculator = new Salaryy();
+        salaryCalculator.getSalary();
+    }
+}
+    
+    
+
